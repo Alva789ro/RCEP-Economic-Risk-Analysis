1d7a15eafc35e800864cad1cfb6e8a1e501c3edc
Alva789ro <alvaromendozaola@outlook.com>
2020-12-17 17:02:16 -0500

    Added the code to make graphs with the data.

--- graphs.py
+++ graphs.py
@@ -1,3 +1,22 @@
 from file_importer0 import australia, brunei, cambodia, china, indonesia, japan, lao, malaysia, myanmar, new_zeland, philipines, singapore, thailand, vietnam
+import matplotlib.pyplot as plt
 
-print(brunei)
+
+
+
+#RGDP_graph = plt.plot(australia['Year'], brunei['RGDP'], ['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP']australia['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP'], australia['RGDP'])
+#plt.show()
+list = [australia, brunei, cambodia, china, indonesia, japan, lao, malaysia, myanmar, new_zeland, philipines, singapore, thailand, vietnam]
+names = ['Australia', 'Brunei', 'Cambodia', 'China', 'Indonesia', 'Japan', 'Lao', 'Malaysia', 'Myanmar', 'New Zealand', 'Philippines', 'Singapore', 'Thailand', 'Vietnam']
+
+def plot_creator(ls, x):
+    for v, y in zip(ls, names):
+        plt.plot(australia['Year'], v[x], label= y)
+    plt.title('%s vs Years'%(x))
+    plt.legend(loc='best')
+    plt.show()
+
+
+
+#Here, write in the second variable the variable that you want to plot for. To know what variables are available, please check the ER diegram, specificallly the columns of the tables from the countries in the SQL database.
+plot_creator(list, 'Net_LB')
